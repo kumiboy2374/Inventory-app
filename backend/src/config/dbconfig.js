a194ff15e5fcfaa93471767f6235bfa9203ba5b1
hayyanhaider0 <hayyan2003@gmail.com>
2025-07-26 16:32:52 -0500

    Backend: Fixed dotenv dependency issue

--- backend/src/config/dbconfig.js
+++ backend/src/config/dbconfig.js
@@ -1,6 +1,7 @@
+require("dotenv").config()
 const mongoose = require("mongoose")
 
-const MONGO_URI = process.env.NEXT_MONGO_URI
+const MONGO_URI = process.env.MONGO_URI
 
 mongoose
 	.connect(MONGO_URI, {
